5305346a9e9940a04a24ce5a0db1eef3c59e617d
Amrit-Bhaskar-abhask10 <abhask10@asu.edu>
2022-10-27 15:28:10 -0700

    pic and amazon info

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,24 +5,24 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "Applied Scientist Intern",
+    cardImage: "assets/images/experience-page/amazon.png",
+    place: "Amazon, Seattle, WA, US",
+    time: "(May, 2022 - Aug, 2022)",
+    desp: "<li>Customer credit model: Worked on enhancing the performance of one of the customer credit ML models. Enhanced the model performance by 1.24% in accuracy and 4.48% in AUC over the existing model in production.</li>",
   },
   {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
+    title: "Senior Data Analyst",
+    cardImage: "assets/images/experience-page/tiger_analytics.png",
+    place: "Tiger Analytics, Bengaluru, India",
+    time: "(Nov, 2019 - July, 2021)",
     desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
   },
   {
     title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
+    cardImage: "assets/images/experience-page/wipro.jpeg",
+    place: "Wipro, Bengaluru, India",
+    time: "(Aug, 2017 - Oct, 2019)",
     desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
   },
 ];
