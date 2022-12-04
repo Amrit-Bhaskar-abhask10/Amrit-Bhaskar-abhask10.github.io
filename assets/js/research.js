ff4efdac7f9cd50ae4fc9c0c5dd3fd395ca5635b
Amrit-Bhaskar-abhask10 <abhask10@asu.edu>
2022-12-04 11:35:18 -0700

    trying research web link

--- assets/js/research.js
+++ assets/js/research.js
@@ -19,6 +19,7 @@ const research = [
     abstract:
       "Distant supervised relation extraction is widely used for finding relational facts from texts, but it is coupled with wrong labeling problem, which in turn hurts the model performance. To address this issue, we present a sentence-level attentionbased model for relation extraction, in which we use convolutional neural networks to get the semantics of sentences. Sentence level attention over multiple instances are built, which in turn significantly reduces the weights of the noisy data. We have tested the system with eKYC data and evaluated the results with respect to ownership relation extraction between organizations.",
     absbox: "absPopup1",
+    web_link: "https://accsindia.org/downloads/ADCOM-2018-papers/ADCOM_2018_paper_35.pdf",
   },
 
   {
@@ -37,6 +38,7 @@ const research = [
     abstract:
       "Combating fake news is one of the burning societal crisis. It is difficult to expose false claims before they create a lot of damage. Automatic fact/claim verification has recently become a topic of interest among diverse research communities. Forums like FEVER, FNC aim to discuss automatic fact-checking on text. Research efforts and datasets on text fact verification could be found, but there is not much attention towards multi-modal or cross-modal fact-verification. In order to bring the attention of the research community towards understanding multimodal misinformation, we release a multimodal fact checking dataset named FACTIFY. It is notably the largest multimodal fact verification public dataset consisting of 50K data points, covering news from India and the US. FACTIFY contains images, textual claims, reference textual documents and images labeled with three broad categories namely - support, no-evidence, and refute.",
     absbox: "absPopup2",
+    web_link: "https://www.researchgate.net/publication/356342935_FACTIFY_A_Multi-Modal_Fact_Verification_Dataset",
   },
 
   {
@@ -55,6 +57,7 @@ const research = [
     abstract:
       "Fake news can spread quickly on social media and it is important to detect it before it creates lot of damage. Automatic fact/claim verification has recently become a topic of interest among diverse research communities. We present the findings of the Factify shared task, which aims undertake multi-modal fact verification, organized as a part of the De-Factify workshop at AAAI’22. The task is modeled as a multi-modal entailment task, where each input needs to be classified into one of 5 classes based on entailment and modality. A total of 64 teams participated in the Factify shared task, and of them, 9 teams submitted their predictions on test set. The most successful models were BigBird or other variations of BERT. The highest F1 score averaged across all the classes was 76.82%.",
     absbox: "absPopup3",
+    web_link: "https://www.researchgate.net/publication/359451712_Benchmarking_Multi-Modal_Entailment_for_Fact_Verification",
   },
 
   {
@@ -74,6 +77,7 @@ const research = [
     abstract:
       "This work revisits the task of training sequence tagging models with limited resources using transfer learning. We investigate several proposed approaches introduced in recent works and suggest a new loss that relies on sentence reconstruction from normalized embeddings. Specifically, our method demonstrates how by adding a decoding layer for sentence reconstruction, we can improve the performance of various baselines. We show improved results on the CoNLL02 NER and UD 1.2 POS datasets and demonstrate the power of the method for transfer learning with low-resources achieving 0.6 F1 score in Dutch using only one sample from it.",
     absbox: "absPopup4",
+    web_link: "https://aclanthology.org/2020.acl-main.239.pdf",
   },
 
 //  {
@@ -126,6 +130,7 @@ const research = [
     abstract:
       "A method and system are described for generating structured relations between words in sentences. The method includes generating encoded hidden state vectors using a single layer bi-directional Long Short Term Memory (LSTM) neural network. The method includes generating current hidden state vectors based on word embedding associated with each word at a time stamp ‘t’. The method includes computing attention distribution of each word based on encoded hidden state vectors and current hidden state vectors. The method includes computing context vector of sentences based on attention distribution of each word and the encoded hidden state vectors. The method includes computing vocabulary distribution at time stamp “t” based on context vector and current hidden state vectors. The method includes computing probability distribution of words based on encoded hidden state vectors, current hidden state vectors, and vocabulary distribution. The method includes generating plurality of structured relations between words based on probability distribution.",
     absbox: "absPopup7",
+    web_link: "https://patents.google.com/patent/US20200285932A1/en",
   },
 //  {
 //    title:
@@ -160,6 +165,7 @@ const fillData = () => {
       citation,
       absbox,
       abstract,
+      web_link,
     }) =>
       (output += `
             <tr data-aos="zoom-in-left"> 
@@ -193,6 +199,11 @@ const fillData = () => {
                         </div>
                     </div>
                     <div id="${citebox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
+                        <div class="card-body">
+                            ${citation.vancouver}    
+                        </div>
+                    </div>
+                    <div id="${web_link}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse" onclick="window.open('${web_link}','_blank');">
                         <div class="card-body">
                             ${citation.vancouver}    
                         </div>
